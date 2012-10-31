a5ca520211e1b2cb21d65127e5c33397f9d62d27
elm <elm@elm.(none)>
2012-10-31 00:21:44 -0400

    added FileUserDatabase to get and save users

---
+++ server/src/main/java/com/ece/superkids/FileUserDatabase.java
@@ -0,0 +1,70 @@
+package com.ece.superkids;
+
+import com.ece.superkids.entities.User;
+import com.google.gson.Gson;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileUserDatabase  {
+  private static final String FILE_NAME_USER = "/Users.txt";
+  private Gson gson;
+  private Map<String, User> users;
+
+  public FileUserDatabase() {
+      gson = new Gson();
+      users = new HashMap<String, User>();
+  }
+
+  private void loadUsersFromFile(String fileName) {
+      users = new HashMap<String, User>();
+      InputStream in = getClass().getResourceAsStream(fileName);
+      try {
+          BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+          while (true) {
+              String line = reader.readLine();
+              if (line == null) {
+                  break;
+              }
+              User user = gson.fromJson(line, User.class);
+              String name = user.getName();
+              users.put(name, user);
+          }
+      } catch(Exception e) {
+
+      }
+  }
+
+  public boolean saveUser(String name) {
+      User user = new User(name);
+      if(!users.containsKey(name)) {
+          File file = new File(getClass().getResource("/" + FILE_NAME_USER).getFile());
+          try {
+            FileWriter writer = new FileWriter(file, true);
+            writer.write(gson.toJson(user));
+            writer.write('\n');
+            writer.close();
+
+            users.put(name, user);
+          } catch(Exception e) {
+              // e.printStackTrace();
+              System.out.println("Error saving user to file");
+              return false;
+          }
+          return true;
+      } else {
+          return false;
+      }
+  }
+
+  public User getUser(String name) {
+      if(users.containsKey(name)) {
+          return users.get(name);
+      } else {
+          return null;
+      }
+  }
+
+}
