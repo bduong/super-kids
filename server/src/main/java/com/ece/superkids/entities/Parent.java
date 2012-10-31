54473f5f0112d73b2a4c76e7c4821f541c13dd62
elm <elm@elm.(none)>
2012-10-31 00:33:00 -0400

    Added parent class, still not done

---
+++ server/src/main/java/com/ece/superkids/entities/Parent.java
@@ -0,0 +1,40 @@
+/**
+ * @author M4rc Adam
+ */
+package com.ece.superkids.entities;
+
+import java.util.ArrayList;
+
+public class Parent {
+    private ArrayList<User> users;
+
+    public Parent() {
+        users = new ArrayList();
+    }
+
+    public ArrayList<User> getUsers() {
+        return users;
+    }
+
+    public boolean addUser(String name) {
+        User user = new User(name);
+        if(!users.contains(user)) {
+            users.add(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean removeUser(String name) {
+        for(int i=0; i<users.size(); i++) {
+            if(users.get(i).getName().equals(name)) {
+                users.remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
