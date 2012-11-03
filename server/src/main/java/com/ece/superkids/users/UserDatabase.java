cc64fdde9aeede2e330b1746e5a3a14553f9075b
elm <elm@elm.(none)>
2012-11-03 15:39:35 -0400

    added delete user and update user to interface

--- server/src/main/java/com/ece/superkids/users/UserDatabase.java
+++ server/src/main/java/com/ece/superkids/users/UserDatabase.java
@@ -5,7 +5,8 @@ import com.ece.superkids.users.entities.User;
 public interface UserDatabase {
 
     public User getUser(String name);
-    
     public void saveUser(User user);
+    public void updateUser(User oldUser, User newUser);
+    public boolean deleteUser(String name);
 
 }
