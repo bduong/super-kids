bdd912bcc823539cf9de274e8a70b28e0dfb49bd
elm <elm@elm.(none)>
2012-11-03 17:44:21 -0400

    added interface to get all users

--- server/src/main/java/com/ece/superkids/users/UserDatabase.java
+++ server/src/main/java/com/ece/superkids/users/UserDatabase.java
@@ -1,10 +1,12 @@
 package com.ece.superkids.users;
 
 import com.ece.superkids.users.entities.User;
+import java.util.ArrayList;
 
 public interface UserDatabase {
 
     public User getUser(String name);
+    public ArrayList<User> getAllUsers();
     public void saveUser(User user);
     public void updateUser(User oldUser, User newUser);
     public boolean deleteUser(String name);
