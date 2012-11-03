d9325987ed954cc906517bfce367a16d43698a67
elm <elm@elm.(none)>
2012-11-03 18:52:32 -0400

    added functions to save state to history, and to get history

--- server/src/main/java/com/ece/superkids/users/entities/History.java
+++ server/src/main/java/com/ece/superkids/users/entities/History.java
@@ -3,6 +3,7 @@
  */
 package com.ece.superkids.users.entities;
 
+import com.ece.superkids.questions.entities.Question;
 import com.ece.superkids.questions.enums.QuestionLevel;
 import com.ece.superkids.questions.enums.QuestionCategory;
 
@@ -14,7 +15,7 @@ import java.util.LinkedList;
 import java.util.Iterator;
 
 public class History {
-    // category and level map //
+    
     private Map<String, ArrayList<State>> questionToList;
 
     public History() {
@@ -22,22 +23,45 @@ public class History {
     }
 
     public void saveToHistory(QuestionCategory category, QuestionLevel level, State state) {
-        String key = category + ":" + level;
+        String key = category.toString() + ":" + level.toString();
         if(questionToList.containsKey(key)) {
-            ArrayList<State> arrayList = questionToList.get(key);
-            if(arrayList.size()>5) {
-                arrayList.remove(0);
-            }
-            arrayList.add(state);
+            // get array
+            ArrayList<State> states = (ArrayList<State>)questionToList.get(key);
+            states.add(state);
         } else {
-            ArrayList<State> arrayList = new ArrayList();
-            arrayList.add(state);
-            questionToList.put(key, arrayList);
+            ArrayList<State> states = new ArrayList();
+            states.add(state);
+            questionToList.put(key, states);
         }
     }
+    
+    public Map<Question, ArrayList<Integer>> getHistory(QuestionCategory category, QuestionLevel level) {
+        String key = category.toString() + ":" + level.toString();
+        Map<Question, ArrayList<Integer>> questionToScores = new HashMap();
+        if(questionToList.containsKey(key)) {
+            ArrayList<State> states = (ArrayList<State>)questionToList.get(key);
 
-    public Map<String, ArrayList<Integer>> getHistory(QuestionCategory category, QuestionLevel level) {
-        return null;
+            for(int i=0; i<states.size(); i++) {
+                Iterator it = states.get(i).getAllScores().entrySet().iterator();
+                while(it.hasNext()) {
+                    Map.Entry pairs = (Map.Entry)it.next();
+                    Question questionKey = (Question)pairs.getKey();
+                    if(questionToScores.containsKey(questionKey)) {
+                        ArrayList<Integer> listOfScores = (ArrayList<Integer>)questionToScores.get(questionKey);
+                        listOfScores.add((Integer)pairs.getValue());
+                        questionToScores.put(questionKey, listOfScores);
+                    } else {
+                        ArrayList<Integer> listOfScores = new ArrayList();
+                        listOfScores.add((Integer)pairs.getValue());
+                        questionToScores.put(questionKey, listOfScores);
+                    }
+                }
+            }
+            return questionToScores;
+            
+        } else {
+            return null;
+        }
     }
 
 }
