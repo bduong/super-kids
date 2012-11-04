5cbeb3ca07dd4fd7291571c76267e9f280ee81bf
Ben Duong <benjaminf.duong@gmail.com>
2012-11-03 23:10:12 -0400

    Revert "Added the Mary TTS Engine"
    This reverts commit c499522a34d307afa81d7e2be84e761cf0b66ce5.

--- GUI/src/main/java/superkidsapplication/tts/SpeechController.java
+++
@@ -1,9 +0,0 @@
-package superkidsapplication.tts;
-
-import marytts.exceptions.MaryConfigurationException;
-import marytts.exceptions.SynthesisException;
-
-public interface SpeechController {
-
-    void say(String speech) throws Exception;
-}
