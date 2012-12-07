846c1172021ce0a11ecdca907e26817248891ed1
elm <elm@elm.(none)>
2012-12-07 12:03:17 -0500

    api: added isLevelFinished() function for user, and deleteUser()

--- server/src/main/java/com/ece/superkids/users/entities/User.java
+++ server/src/main/java/com/ece/superkids/users/entities/User.java
@@ -96,6 +96,9 @@ public class User implements Serializable {
     public boolean isCurrentLevelFinished(){
         return history.isLevelFinished(this.state.getCurrentLevel());
     }
+    public boolean isLevelFinished(QuestionLevel level) {
+        return history.isLevelFinished(level);
+    }
 
     public void setCurrentQuestion(Question question) {
         history.setGameStarted();
@@ -151,6 +154,11 @@ public class User implements Serializable {
     public void saveUser() {
         (new FileUserManager()).addUser(this);
         (new FileUserManager()).updateUser(this, this);
+    }
+
+    /* deletes the serializable file for the user */
+    public void deleteUser() {
+        (new FileUserManager()).deleteUser(name);
     }
 
     public Object[][] getHistory(QuestionCategory questionCategory, QuestionLevel questionLevel) {
