55d5e1e652d7a2e4bf5e4baa602454ec06ff7912
btevfik <bt@bu.edu>
2012-12-12 00:15:32 -0500

    added a music button in the main frame to easily access to the music controls

--- GUI/src/main/java/com/ece/superkids/ui/frames/MainFrame.java
+++ GUI/src/main/java/com/ece/superkids/ui/frames/MainFrame.java
@@ -4,6 +4,7 @@
  */
 package com.ece.superkids.ui.frames;
 
+import com.ece.superkids.ui.controllers.MusicController;
 import com.ece.superkids.ui.user.frames.LogoutAuthFrame;
 import com.ece.superkids.ui.user.panels.UserSelectionPanel;
 import com.ece.superkids.ui.parent.frames.AuthFrame;
@@ -12,24 +13,25 @@ import com.ece.superkids.users.ParentManager;
 import com.ece.superkids.users.UserDatabaseFactory;
 import com.ece.superkids.ui.controllers.PanelController;
 import com.ece.superkids.ui.events.Session;
+import com.ece.superkids.ui.user.panels.SoundPanel;
 
 /**
- * Creates new form <code>MainFrame</code>
- * This is the main frame for the application
- * 
+ * Creates new form
+ * <code>MainFrame</code> This is the main frame for the application
+ *
  * @author david cheung
  */
 public class MainFrame extends javax.swing.JFrame {
 
-    
     //get the panel controller to manage panels
     private PanelController controller;
     private ParentManager pM;
     private Session session = Session.aSession();
+    private MusicController mController = MusicController.getInstance();
+    private boolean musicOn = false;
 
     /**
-     * Creates new form MainFrame
-     * This is the main frame for the application
+     * Creates new form MainFrame This is the main frame for the application
      */
     public MainFrame() {
         initComponents();
@@ -50,9 +52,13 @@ public class MainFrame extends javax.swing.JFrame {
         } else if (!pM.doesParentExist()) {
             controller.addPanel(new InitialSetupPanel());
         }
-        
+
         //BACK ITEM DISABLED
         BackItem.setVisible(false);
+        
+        //add soundControl panel
+        soundControls.setVisible(false);
+        soundControls.add(SoundPanel.getInstance());
     }
 
     /**
@@ -66,6 +72,9 @@ public class MainFrame extends javax.swing.JFrame {
 
         logoutButton = new javax.swing.JButton();
         contentArea = new javax.swing.JPanel();
+        jLayeredPane2 = new javax.swing.JLayeredPane();
+        musicButton = new javax.swing.JButton();
+        soundControls = new javax.swing.JPanel();
         jMenuBar1 = new javax.swing.JMenuBar();
         FileMenu = new javax.swing.JMenu();
         BackItem = new javax.swing.JMenuItem();
@@ -95,6 +104,28 @@ public class MainFrame extends javax.swing.JFrame {
         setResizable(false);
 
         contentArea.setLayout(new java.awt.BorderLayout());
+
+        musicButton.setFont(new java.awt.Font("Lucida Grande", 0, 10)); // NOI18N
+        musicButton.setText("Music");
+        musicButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                musicButtonActionPerformed(evt);
+            }
+        });
+        musicButton.setBounds(0, 0, 60, 20);
+        jLayeredPane2.add(musicButton, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        soundControls.setBackground(new java.awt.Color(51, 153, 255));
+        soundControls.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                soundControlsMouseClicked(evt);
+            }
+        });
+        soundControls.setBounds(0, 0, 330, 370);
+        jLayeredPane2.add(soundControls, javax.swing.JLayeredPane.DEFAULT_LAYER);
+
+        contentArea.add(jLayeredPane2, java.awt.BorderLayout.CENTER);
+
         getContentPane().add(contentArea, java.awt.BorderLayout.CENTER);
 
         jMenuBar1.setMaximumSize(new java.awt.Dimension(800, 22));
@@ -210,7 +241,7 @@ public class MainFrame extends javax.swing.JFrame {
     private void LearnModeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LearnModeActionPerformed
         // TODO add your handling code here:
 //        SubjectSelectionPanel sPanel = new SubjectSelectionPanel();
-       controller.goToLearning();
+        controller.goToLearning();
     }//GEN-LAST:event_LearnModeActionPerformed
 
     private void ExitAppItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ExitAppItemActionPerformed
@@ -222,6 +253,25 @@ public class MainFrame extends javax.swing.JFrame {
         // TODO add your handling code here:
         new LogoutAuthFrame(logoutButton).setVisible(true);
     }//GEN-LAST:event_logoutButtonActionPerformed
+
+    private void musicButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_musicButtonActionPerformed
+        // TODO add your handling code here:
+        if (musicOn == false) {
+            soundControls.setVisible(true);
+            musicOn = true;
+            return;
+        }
+        if (musicOn == true) {
+            soundControls.setVisible(false);
+            musicOn = false;
+        }
+    }//GEN-LAST:event_musicButtonActionPerformed
+
+    private void soundControlsMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_soundControlsMouseClicked
+        // TODO add your handling code here:
+        soundControls.setVisible(false);
+        musicOn=false;
+    }//GEN-LAST:event_soundControlsMouseClicked
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JMenuItem AboutItem;
@@ -236,7 +286,10 @@ public class MainFrame extends javax.swing.JFrame {
     public javax.swing.JMenuItem ParentItem;
     public javax.swing.JMenuItem QMode;
     public javax.swing.JPanel contentArea;
+    private javax.swing.JLayeredPane jLayeredPane2;
     private javax.swing.JMenuBar jMenuBar1;
     public javax.swing.JButton logoutButton;
+    private javax.swing.JButton musicButton;
+    private javax.swing.JPanel soundControls;
     // End of variables declaration//GEN-END:variables
 }
